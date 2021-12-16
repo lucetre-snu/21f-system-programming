19d0dc7b8696ab54753d5f5e8cc19935912906cf
Sangjun Son <lucetre97@gmail.com>
2021-12-16 15:33:43 +0900

    End of Fall semester

---
+++ Resource/test/.ipynb_checkpoints/test3-checkpoint.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+int main() {
+  printf("Hello\n");
+  printf("Bye");
+  return 0;
+}
---
+++ Resource/test/test3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+
+int main() {
+  pthread_t tid[2];
+  sem_init(&mutex[0], 0, 1);
+  
+  return 0;
+}
